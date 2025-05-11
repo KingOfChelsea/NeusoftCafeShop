49ae057b91b4e2ed2d30a1513648365346a9166a
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-05-12 00:18:55 +0800

    目的：猜你喜欢模块组件架构的封装
    Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -1,7 +1,10 @@
 import type NeusoftSwiper from './NeusoftSwiper.vue'
-
+import type NeusoftRecordment from './NeusoftRecordment.vue'
+import type NeusoftGuess from './NeusoftGuess.vue'
 declare module 'vue' {
   export interface GlobalComponents {
     NeusoftSwiper: typeof NeusoftSwiper
+    NeusoftRecordment: typeof NeusoftRecordment
+    NeusoftGuess: typeof NeusoftGuess
   }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,25 +1,61 @@
 <template>
-  <CustomNavbar></CustomNavbar>
-  <NeusoftSwiper :list="bannenrList"></NeusoftSwiper>
+  <NeusoftCustomNavbar></NeusoftCustomNavbar>
+  <scroll-view class="scroll-view" scroll-y>
+    <NeusoftSwiper :list="bannenrList"></NeusoftSwiper>
+    <NeusoftCategoryPanel :list="categoryList"></NeusoftCategoryPanel>
+    <NeusoftHotPanel :list="hotList"></NeusoftHotPanel>
+    <NeusoftGuess></NeusoftGuess>
+  </scroll-view>
 </template>
 
 <script setup lang="ts">
-import CustomNavbar from './components/CustomNavbar.vue'
+import NeusoftCustomNavbar from './components/NeusoftCustomNavbar.vue'
+import NeusoftCategoryPanel from './components/NeusoftCategoryPanel.vue'
+import NeusoftHotPanel from './components/NeusoftHotPanel.vue'
 import { getHomeBannerAPI } from '@/services/NeusoftHomeBanner'
+import { getCategoryAPI } from '@/services/NeusoftHomeCategory'
+import type { NeusoftBannerItem } from '@/types/NeusoftBannerItem'
+import type { NeusoftCategoryItem } from '@/types/NeusoftCategoryItem'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { NeusoftBannerItem } from '@/types/NeusoftBannerItem'
+import { getHotAPI } from '@/services/NeusoftHomeHot'
+import type { NeusoftHotItem } from '@/types/NeusoftHotItem'
 
+// 1.变量/属性定义
 const bannenrList = ref<NeusoftBannerItem[]>([])
+const categoryList = ref<NeusoftCategoryItem[]>([])
+const hotList = ref<NeusoftHotItem[]>([])
+
+//2.数据加载
 const Neusoft_getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
-  console.log(res)
   bannenrList.value = res.result
 }
-//页面加载的时候
+const Neusoft_getHomeCategoryData = async () => {
+  const res = await getCategoryAPI()
+  categoryList.value = res.result
+}
+const Neusoft_getHomeHotDataw = async () => {
+  const res = await getHotAPI()
+  hotList.value = res.result
+}
+
+//页面加载
 onLoad(() => {
   Neusoft_getHomeBannerData()
+  Neusoft_getHomeCategoryData()
+  Neusoft_getHomeHotDataw()
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
+}
+</style>
