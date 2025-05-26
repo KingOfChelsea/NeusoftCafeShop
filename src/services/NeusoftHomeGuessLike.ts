0f3e61290ad6ada149f149e963be42bfc8222382
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-05-27 00:11:25 +0800

    1

--- src/services/NeusoftHomeGuessLike.ts
+++ src/services/NeusoftHomeGuessLike.ts
@@ -6,7 +6,7 @@ import { http } from '@/utils/http'
 export const getGuessLikeAPI = (data?: NeusoftPageParams) => {
   return http<PageResult<NeusoftGuessLikeItem>>({
     method: 'GET',
-    url: '/home/goods/guessLike',
+    url: '/home/goods/guessLikes',
     data,
   })
 }
