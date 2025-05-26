a49ab1f5939b58ff919b9c7204fb1ad7f36f9abf
KingOfChelsea <xuzhenyu3333@gmail.com>
2025-05-26 23:58:06 +0800

    目的：1.调整获取猜你喜欢数据的方法
    Signed-off-by: KingOfChelsea <xuzhenyu3333@gmail.com>

--- src/services/NeusoftHomeGuessLike.ts
+++ src/services/NeusoftHomeGuessLike.ts
@@ -1,10 +1,12 @@
 import type { PageResult } from '@/types/global'
 import type { NeusoftGuessLikeItem } from '@/types/NeusoftGuessItem'
+import type { NeusoftPageParams } from '@/types/NeusoftPageParams'
 import { http } from '@/utils/http'
 
-export const getGuessLikeAPI = () => {
+export const getGuessLikeAPI = (data: NeusoftPageParams) => {
   return http<PageResult<NeusoftGuessLikeItem>>({
     method: 'GET',
     url: '/home/goods/guessLike',
+    data,
   })
 }
